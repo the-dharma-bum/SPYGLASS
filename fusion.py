813bc84e41f9019d55af588e252bcf6690cdd609
Luc Vedrenne <luc.vedrenne@imt-atlantique.net>
2020-10-15 01:24:05 +0200

    add docstrings in fusion

--- fusion.py
+++ fusion.py
@@ -3,8 +3,29 @@ import torch.nn as nn
 
 class Fusion(nn.Module):
 
+    """ Pytorch Implementation of:
+        Large-scale Video Classification with Convolutional Neural Networks.
+    
+        Implements:
+            - single frame
+            - early fusion
+            - late fusion
+    """
+
     def __init__(self, net: nn.Module,
                  hidden_dim1: int=2048, hidden_dim2: int=512, num_classes: int=2) -> None:
+        """ Initalise a fusion environnement.
+        
+        We need to remove the last fully connected layer of the base network and replace it
+        by our own clasifier.
+        Hence the two hidden dimensions and the num classes.
+
+        Args:
+            net (nn.Module): a base network on which to perform fusion.
+            hidden_dim1 (int, optional): Classifier's first hidden dimension. Defaults to 2048.
+            hidden_dim2 (int, optional): Classifier's second hidden dimension. Defaults to 512.
+            num_classes (int, optional): Last fully connected layer output dimension. Defaults to 2.
+        """
         super().__init__()
         self.net = net
         self.num_classes = num_classes
@@ -15,24 +36,57 @@ class Fusion(nn.Module):
         self.bn2 = nn.BatchNorm1d(hidden_dim2, momentum=0.01)
         self.fc3 = nn.Linear(hidden_dim2, self.num_classes)
 
-    def init_early_fusion(self, early_fusion_hidden_dim : int, window: int):
+    def init_early_fusion(self, early_fusion_hidden_dim : int, window: int) -> None:
+        """ Prepare the network to perfom an early fusion.
+        As we take a 3d inputs (of depth called window),we replace the first Conv2d
+        of the base network by the following:
+            1. a Conv3d taking with 3 inputs channels and a output dim defined by
+               early_fusion_hidden_dim.
+            2. a Conv2d with an input dim equals to early_fusion_hidden_dim and an output 
+               dim of 64 so that it matches the output dim of the initial Conv2d.
+
+        Args:
+            early_fusion_hidden_dim (int):  the intermediate state's hidden dim. 
+            window (int): how many consecutive frames do we fuse.
+        """
         self.conv3d = nn.Conv3d(3,early_fusion_hidden_dim, kernel_size=3, padding=1)
         self.net.features[0] = nn.Conv2d(early_fusion_hidden_dim * window, 64,
-                                         kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+                                         kernel_size=(7, 7), stride=(2, 2),
+                                         padding=(3, 3), bias=False)
         if torch.cuda.is_available():
             self.conv3d = self.conv3d.cuda()
             self.net.features[0] = self.net.features[0].cuda()
     
     def init_first_fully_connected(self, num_output_filters: int) -> None:
+        """ Initialise the first fully connected of self.classifier.
+
+        When doing late fusion, we concatenate to frames outputs at the end
+        of the network so we need to adust the first fully connected layer
+        input dim accordingly.
+
+        Args:
+            num_output_filters (int): How many filters where outputed by the 
+                                      base network before its first fc.
+        """
         self.fc1 = nn.Linear(num_output_filters, self.hidden_dim1)
         if torch.cuda.is_available():
             self.fc1 = self.fc1.cuda()
 
     def remove_last_fully_connected(self):
+        """ Removes the last layer of the base network. """
         modules  = list(self.net.children())[:-1]
         self.net = nn.Sequential(*modules)
 
     def classifier(self, x: torch.Tensor) -> torch.Tensor:
+        """ Three stages of (BatchNorm + FC) to classify a single frame.
+
+        Args:
+            x (torch.Tensor): The output of a network without its
+                              last fc layer.
+
+        Returns:
+            torch.Tensor: The output for one given frame, of shape (batc_size, num_classes).
+        """
         num_output_filters = x.size(1)
         self.init_first_fully_connected(num_output_filters)
         x = self.bn1(self.fc1(x))
@@ -41,6 +95,17 @@ class Fusion(nn.Module):
         return outputs
     
     def single_frame(self, x: torch.Tensor) -> torch.Tensor:
+        """ Single Frame Fusion.
+
+        Predicts all frames independantly and outputs the mean output
+        of all frame.
+
+        Args:
+            x (torch.Tensor): A 5d tensor: (batch_size, channesl, time_depth, x_size, y_size)
+                              
+        Returns:
+            torch.Tensor: The final output, of shape (batc_size, num_classes).
+        """
         batch_size, time_depth = x.size(0), x.size(2)
         outputs = torch.FloatTensor(time_depth, batch_size, self.num_classes)
         for t in range(time_depth):
@@ -48,6 +113,18 @@ class Fusion(nn.Module):
         return outputs.mean(axis=0)
 
     def late_fusion(self, x: torch.Tensor, distance: int=5) -> torch.Tensor:
+        """ Remove the last fully connected layer. 
+        Encode two frames separated by a defined distance (pass through the network
+        minus the last fc layer). 
+        Concatenate the encoded frames.
+        Use self.classier on that concatenated tensor.
+        Args:
+            x (torch.Tensor): A 5d tensor: (batch_size, channesl, time_depth, x_size, y_size)
+            distance (int, optional): How far apart are the two frames to fuse. Defaults to 5.
+
+        Returns:
+            torch.Tensor: The final output, of shape (batc_size, num_classes).
+        """
         self.remove_last_fully_connected()
         batch_size, time_depth = x.size(0), x.size(2)
         outputs = torch.FloatTensor(time_depth, batch_size, self.num_classes)
@@ -61,16 +138,30 @@ class Fusion(nn.Module):
 
     def early_fusion(self, x: torch.Tensor, early_fusion_hidden_dim: int=3,
                      window: int=4, stride: int=1) -> torch.Tensor:
+        """
+        Fuse a window of consecutive frames at the beginning of the network
+        by doing a Conv3d followed by a flattening followed by a Conv2d.
+
+        Args:
+            x (torch.Tensor): A 5d tensor: (batch_size, channesl, time_depth, x_size, y_size)
+            early_fusion_hidden_dim (int, optional): Output dim of the Conv3d. Defaults to 3.
+            window (int, optional): How many consecutive frames to fuse. Defaults to 4.
+            stride (int, optional): Stride between each window. Defaults to 1.
+
+        Returns:
+            torch.Tensor: The final output, of shape (batc_size, num_classes). 
+        """
         self.init_early_fusion(early_fusion_hidden_dim, window)
         batch_size, time_depth = x.size(0), x.size(2)
         outputs = torch.FloatTensor(time_depth, batch_size, self.num_classes)
         for t in range(0,time_depth-window,stride): 
-            # early fusion
+            # shape before get window: (batch_size, num_channels, time_depth, x_size, y_size) 
             frame_window = x.narrow(2,t,window)
+            # shape after get window: (batch_size, num_channels, window, x_size, y_size)
             embedded_vector = self.conv3d(frame_window)
-            # shape after conv_3d: (batch_size,early_fusion_hidden_dim,windows,x_size,y_size)
+            # shape after conv_3d: (batch_size, early_fusion_hidden_dim, window, x_size, y_size)
             embedded_vector = torch.flatten(embedded_vector, start_dim=1, end_dim=2) # IS THIS OK ??? 
-            # shape after flattening: (batch_size, early_fusion_hidden_dim*windows, x_size, y_size)
+            # shape after flattening: (batch_size, early_fusion_hidden_dim * windows, x_size, y_size)
             outputs[t,:,] = self.net(embedded_vector)
         return outputs.mean(axis=0)
 
--- test_fusion.py
+++ test_fusion.py
@@ -3,18 +3,23 @@ import torch
 import torch.nn as nn
 from fusion import Fusion
 
-X_SIZE = 200
-Y_SIZE = 200
-NUM_CHANNELS = 3
-BATCH_SIZE = 2
 
+# Batch size
+BATCH_SIZE   = 2
+NUM_CHANNELS = 3
+TIME_DEPTH   = 10
+X_SIZE       = 200
+Y_SIZE       = 200
+
+# Classifier (for late fusion)
 NUM_CLASSES = 2
 HIDDEN_DIM1 = 2048
 HIDDEN_DIM2 = 512
 
+# Fusion params
 DISTANCE    = 5  # late fusion 
 WINDOW      = 10 # early fusion
-STRIDE      = 1
+STRIDE      = 1  # early fusion
 EARLY_FUSION_HIDDEN_DIM = 3
 
 
@@ -22,11 +27,13 @@ EARLY_FUSION_HIDDEN_DIM = 3
 net = torch.hub.load('pytorch/vision:v0.7.0', 'densenet121', 
                      pretrained=False, num_classes=NUM_CLASSES).cuda()
 # Shape : (batch_size, channels, time_depth, x_size, y_size)
-dummy_tensor = torch.FloatTensor(BATCH_SIZE,NUM_CHANNELS,10,X_SIZE,Y_SIZE).cuda()
+dummy_tensor = torch.FloatTensor(BATCH_SIZE, NUM_CHANNELS, TIME_DEPTH, X_SIZE, Y_SIZE).cuda()
 
 
 
-print(f'Expected Shape: ({BATCH_SIZE}, {NUM_CLASSES})')
+print(f'Expected Shape: [{BATCH_SIZE}, {NUM_CLASSES}]')
+
+# We redefine fusion to reset the base network.
 
 fusion = Fusion(net).cuda()
 single_frame_outputs = fusion.single_frame(dummy_tensor)
